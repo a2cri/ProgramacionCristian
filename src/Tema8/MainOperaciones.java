4df260805466131d24a2b29bccc8eb00758bc158
Cri <crisdejesusguinot11@gmail.com>
2026-04-30 12:00:56 +0200

    Add files via upload

--- src/Tema8/MainOperaciones.java
+++ src/Tema8/MainOperaciones.java
@@ -2,23 +2,118 @@ package Tema8;
 
 import java.util.*;
 
-import static Tema8.Operaciones.consultarEstudiante;
+import static Tema8.Operaciones.*;
 
 public class MainOperaciones {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("Menú de Operaciones " +
+                    "\n 1. Listado de Asignaturas " +
+                    "\n 2. Consultar los estudiantes de una casa " +
+                    "\n 3. Obtener mascota de estudiante " +
+                    "\n 4. Numero de estudiantes por casa " +
+                    "\n 5. Insertar nueva asignatura" +
+                    "\n 6. Modificar el aula de una Asignatura" +
+                    "\n 7. Eliminar asignatura" +
+                    "\n 0. Salir");
+            System.out.println("Selecciona una opción");
+            opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
+                case 1:
+                    List<Asignatura> listaAsignaturas = Operaciones.listarAsignaturas();
 
-        List<Asignatura> listaAsignaturas = Operaciones.listarAsignaturas();
+                    System.out.println("--- LISTADO DE TODAS LAS ASIGNATURAS ---");
+                    for (Asignatura a : listaAsignaturas) {
+                        System.out.println(a);
+                    }
+                    System.out.println("Pulsa ENTER para salir");
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
+                case 2:
 
-        System.out.println("--- LISTADO DE TODAS LAS ASIGNATURAS ---");
-        for (Asignatura a : listaAsignaturas) {
-            System.out.println(a);
-        }
+                    System.out.println("Introduce la casa que desea saber que alumnos tiene: ");
+                    String casaBusqueda = sc.nextLine();
+                    consultarEstudiante(casaBusqueda);
+                    System.out.println("Pulsa ENTER para salir");
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
+                case 3:
+                    System.out.println("Introduce el nombre del alumno que quieres buscar(Despues se te preguntara por el apellido de este)");
+                    String nombre = sc.nextLine();
+                    System.out.println("Introduce el apellido del alumno introducido anteriormente");
+                    String apellido = sc.next();
 
-        Scanner sc=new Scanner(System.in);
+                    mascotaEstudiante(nombre, apellido);
+                    System.out.println("Pulsa ENTER para salir");
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
+                case 4:
+                    Operaciones.numeroEstudiantes();
+                    System.out.println("Pulsa ENTER para salir");
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
+                case 5:
+                    System.out.println("Introduce el nombre de la asignatura: ");
+                    String nombreasignatura = sc.next();
+                    System.out.println("Introduce el aula en la que se enseña esta asignatura: ");
+                    String aulaasignatura = sc.next();
 
-        System.out.println("Introduce la casa que desea saber que alumnos tiene: ");
-        String casaBusqueda=sc.nextLine();
-        consultarEstudiante(casaBusqueda);
+                    boolean obligatori = false;
+                    boolean entradaValida = false;
+                    do {
+                        System.out.println("¿Es su asignatura obligatoria? Si o No");
+                        String respuesta = sc.nextLine().toLowerCase();
+                        if (respuesta.equals("si")) {
+                            obligatori = true;
+                            entradaValida = true;
+                        } else if (respuesta.equals("no")) {
+                            obligatori = false;
+                            entradaValida = true;
+                        } else {
+                            System.out.println("Respuesta no válida.");
+                        }
+                    } while (!entradaValida);
+                    Operaciones.insertarAsignatura(nombreasignatura, aulaasignatura, obligatori);
+                    System.out.println("--- TABLA ACTUALIZADA TRAS INSERCIÓN ---");
+                    List<Asignatura> listaPostInsert = Operaciones.listarAsignaturas();
+                    for (Asignatura a : listaPostInsert) {
+                        System.out.println(a);
+                    }
+                    System.out.println("Pulsa ENTER para salir");
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
+                case 6:
+                    System.out.println("Introduce el id de la asignatura que quieres modificar: ");
+                    int idasignatura = sc.nextInt();
+                    System.out.println("Introduce el nombre de la nueva aula (ej: Aula de Alquimia): ");
+                    String nuevaaula = sc.next();
+                    Operaciones.modificarAulaAsignatura(nuevaaula, idasignatura);
+                    System.out.println("--- TABLA ACTUALIZADA TRAS MODIFICACIÓN ---");
+                    for (Asignatura a : Operaciones.listarAsignaturas()) {
+                        System.out.println(a);
+                    }
+                    System.out.println("Pulsa ENTER para salir");
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
+                case 7:
+                    System.out.println("Introduce el id de la asignatura que desea eliminar: ");
+                    int idasignaturaa = sc.nextInt();
+                    Operaciones.eliminarAsignatura(idasignaturaa);
+                    System.out.println("Pulsa ENTER para salir");
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
+            }
 
+        } while (opcion != 0);
     }
 }
--- src/Tema8/Operaciones.java
+++ src/Tema8/Operaciones.java
@@ -3,12 +3,13 @@ package Tema8;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class Operaciones {
+//Actividad 0
     public static List<Asignatura> listarAsignaturas() {
         List<Asignatura> lista = new ArrayList<>();
-        String sql = "SELECT id_asignatura, nombre_asignatura, aula, obligatoria FROM Asignatura";
+        String sql = "SELECT * FROM Asignatura";
 
 
         try (Connection con = DriverManager.getConnection("jdbc:postgresql://ad-postgres.c7dcxanpavyz.us-east-1.rds.amazonaws.com:5432/hogwarts", "postgres", "12345678");
@@ -33,10 +34,10 @@ public class Operaciones {
 
         return lista;
     }
-
+    //Actividad 1
     public static void consultarEstudiante(String nombreCasa) {
 
-        String sql="SELECT nombre, apellido FROM Estudiante JOIN Casa ON Estudiante.id_casa=Casa.id_casa WHERE  nombre_casa= ?";
+        String sql = "SELECT nombre, apellido FROM Estudiante JOIN Casa ON Estudiante.id_casa=Casa.id_casa WHERE  nombre_casa= ?";
 
         try (Connection con = DriverManager.getConnection("jdbc:postgresql://ad-postgres.c7dcxanpavyz.us-east-1.rds.amazonaws.com:5432/hogwarts", "postgres", "12345678");
              PreparedStatement ps = con.prepareStatement(sql)) {
@@ -48,12 +49,104 @@ public class Operaciones {
                     System.out.println(rs.getString("nombre") + " " + rs.getString("apellido"));
                 }
                 if (!encontrado) {
-                    System.out.println("No hay alumnos en la casa: "+nombreCasa);
+                    System.out.println("No hay alumnos en la casa: " + nombreCasa);
                 }
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
+    //Actividad 2
+    public static void mascotaEstudiante(String nombre, String apellido) {
+        String sql = "SELECT m.nombre_mascota FROM Mascota m JOIN Estudiante e ON m.id_estudiante=e.id_estudiante WHERE e.nombre= ? AND e.apellido=?";
+
+        try (Connection con = DriverManager.getConnection("jdbc:postgresql://ad-postgres.c7dcxanpavyz.us-east-1.rds.amazonaws.com:5432/hogwarts", "postgres", "12345678");
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, nombre);
+            ps.setString(2, apellido);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    String nombreMascota = rs.getString("nombre_mascota");
+                    System.out.println("La mascota de " + nombre + " " + apellido + " es: " + nombreMascota);
+                } else {
+                    System.out.println(nombre + " " + apellido + " no tiene mascotas");
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //Actividad 3
+    public static void numeroEstudiantes() {
+        String sql = "SELECT Casa.nombre_casa, COUNT(Estudiante.id_estudiante) AS total " +
+                "FROM Casa " +
+                "LEFT JOIN Estudiante ON Casa.id_casa = Estudiante.id_casa " +
+                "GROUP BY Casa.nombre_casa";
+        try (Connection con = DriverManager.getConnection("jdbc:postgresql://ad-postgres.c7dcxanpavyz.us-east-1.rds.amazonaws.com:5432/hogwarts", "postgres", "12345678");
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                String casa = rs.getString("nombre_casa");
+                int total = rs.getInt("total");
+                System.out.println(casa + " :" + total);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //Actividad 4
+    public static void insertarAsignatura(String nombre, String aula, boolean obligatoria) {
+
+        String sql="INSERT INTO Asignatura(nombre_asignatura, aula, obligatoria) VALUES ( ?, ?, ?)";
+       try(Connection con= DriverManager.getConnection("jdbc:postgresql://ad-postgres.c7dcxanpavyz.us-east-1.rds.amazonaws.com:5432/hogwarts", "postgres", "12345678");
+        PreparedStatement ps= con.prepareStatement(sql)){
+           ps.setString(1, nombre);
+           ps.setString(2, aula);
+           ps.setBoolean(3, obligatoria);
+
+           int filasAfectadas= ps.executeUpdate();
+           if (filasAfectadas>0) {
+               System.out.println("Asignatura "+nombre+" insertada correctamente");
+           }
+       } catch (SQLException ex){
+           System.out.println(ex.getMessage());
+       }
+    }
+
+    public static void modificarAulaAsignatura(String aulaNueva, int idAsignatura){
+
+        String sql= "UPDATE Asignatura SET aula=? WHERE id_asignatura=?";
+        try(Connection con2= DriverManager.getConnection("jdbc:postgresql://ad-postgres.c7dcxanpavyz.us-east-1.rds.amazonaws.com:5432/hogwarts", "postgres", "12345678");
+            PreparedStatement ps= con2.prepareStatement(sql)) {
+            ps.setString(1, aulaNueva);
+            ps.setInt(2, idAsignatura);
+            int filasAfectadas= ps.executeUpdate();
+            if (filasAfectadas>0) {
+                System.out.println("El aula de la asignatura con ID " + idAsignatura + " ha sido cambiada a: " + aulaNueva);
+            }else {
+                System.out.println("No se encontro ninguna asignatura con el id: "+idAsignatura);
+            }
+        }catch (SQLException ex){
+            System.out.println("Error al modificar asignatura "+ex.getMessage());
+        }
+    }
+
+    public static void eliminarAsignatura(int idAsignatura){
+        String sql= "DELETE FROM Asignatura WHERE id_asignatura=?";
+        try(Connection con2= DriverManager.getConnection("jdbc:postgresql://ad-postgres.c7dcxanpavyz.us-east-1.rds.amazonaws.com:5432/hogwarts", "postgres", "12345678");
+        PreparedStatement ps= con2.prepareStatement(sql)){
+            ps.setInt(1,idAsignatura);
+
+            int filasAfectadas= ps.executeUpdate();
+            if (filasAfectadas>0) {
+                System.out.println("Asignatura eliminada correctamente");
+            }else {
+                System.out.println("No se encontro ninguna asignatura identificada por el id "+idAsignatura);
+            }
+        }catch (SQLException ex){
+            System.out.println("Error al eliminar asignatura "+ex.getMessage());
+        }
+    }
+
 }
